ec999e1ffabfcf41d9f9613ed59356dbfdb33642
Donald Dansereau <donald.dansereau@gmail.com>
2025-08-27 12:26:26 +1000

    updated following lab instructions

--- LabExampleSolutions/Lab1/A1.CruftyCode/stars.cpp
+++ LabExampleSolutions/Lab1/A1.CruftyCode/stars.cpp
@@ -11,21 +11,9 @@ const int STAR_COUNT = 100;
 
 class Star {
     public:
-        Star() {
-            reset();
-        }
-
-        void update() {
-            y += velocity;
-            if (y >= SCREEN_HEIGHT) {
-                reset();
-            }
-        }
-
-        void draw() {
-            moveCursor((int)y, x);
-            std::cout << symbol;
-        }
+        Star();
+        void update();
+        void draw();
 
     private:
         int x;
@@ -34,27 +22,14 @@ class Star {
         char symbol;
 
         void reset();
-        void moveCursor(int row, int col) {
-            std::cout << "\033[" << row + 1 << ";" << col + 1 << "H";
-        }
+        void moveCursor(int row, int col);
 };
 
 class StarField {
     public:
-        StarField(int count) {
-            for (int i = 0; i < count; ++i)
-                stars.push_back(Star());
-        }
-
-        void update() {
-            for (auto& s : stars)
-                s.update();
-        }
-
-        void draw() {
-            for (auto& s : stars)
-                s.draw();
-        }
+        StarField(int count);
+        void update();
+        void draw();
         
     private:
         std::vector<Star> stars;
@@ -62,21 +37,10 @@ class StarField {
 
 class Screen {
     public:
-        void clear() {
-            std::cout << "\033[2J";
-        }
-
-        void hideCursor() {
-            std::cout << "\033[?25l";
-        }
-
-        void showCursor() {
-            std::cout << "\033[?25h";
-        }
-
-        void screenflush() {
-            std::cout << std::flush;
-        }
+        void clear();
+        void hideCursor();
+        void showCursor();
+        void screenflush();
 };
 
 int main() {
@@ -98,10 +62,61 @@ int main() {
     return 0;
 }
 
+Star::Star() {
+    reset();
+}
+
+void Star::update() {
+    y += velocity;
+    if (y >= SCREEN_HEIGHT) {
+        reset();
+    }
+}
+
+void Star::draw() {
+    moveCursor((int)y, x);
+    std::cout << symbol;
+}
+
 void Star::reset() {
 x = rand() % SCREEN_WIDTH;
 y = 0;
 velocity = 0.1f + static_cast<float>(rand()) / RAND_MAX * 0.5f;
 symbol = (rand() % 3 == 0) ? '*' : '.';
+}
+
+void Star::moveCursor(int row, int col) {
+    std::cout << "\033[" << row + 1 << ";" << col + 1 << "H";
+}
+
+StarField::StarField(int count) {
+    for (int i = 0; i < count; ++i)
+        stars.push_back(Star());
+}
+
+void StarField::update() {
+    for (auto& s : stars)
+        s.update();
+}
+
+void StarField::draw() {
+    for (auto& s : stars)
+        s.draw();
+}
+
+void Screen::clear() {
+    std::cout << "\033[2J";
+}
+
+void Screen::hideCursor() {
+    std::cout << "\033[?25l";
+}
+
+void Screen::showCursor() {
+    std::cout << "\033[?25h";
+}
+
+void Screen::screenflush() {
+    std::cout << std::flush;
 }
 
