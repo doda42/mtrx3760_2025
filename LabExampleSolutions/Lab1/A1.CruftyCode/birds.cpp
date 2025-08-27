ccc04b9966389564a254e7377c98ed71f2eab71a
Donald Dansereau <donald.dansereau@gmail.com>
2025-08-27 12:38:54 +1000

    updated following lab instructions

--- LabExampleSolutions/Lab1/A1.CruftyCode/birds.cpp
+++ LabExampleSolutions/Lab1/A1.CruftyCode/birds.cpp
@@ -3,10 +3,10 @@
 #include <ctime>
 #include <unistd.h> // for usleep
 
-#define WIDTH (40)
-#define HEIGHT (20)
-#define NUM_mBirds (10)
-#define DELAY (100000) // microseconds between frames
+const int WIDTH = 40;
+const int HEIGHT = 20;
+const int NUM_mBirds = 10;
+const int DELAY = 100000; // microseconds between frames
 
 class Bird {
 public:
@@ -14,53 +14,68 @@ public:
     void move_random();
     int get_x();
     int GetY();
+    
 private:
-    int x, y;
+    int x; 
+    int y;
 };
+
 class CFlock {
 public:
     void MoveAll();
     void draw();
+    
 private:
     Bird mBirds[NUM_mBirds];
 };
+
 // ------------------------- main -------------------------
 int main() {
     std::srand(std::time(0));
     CFlock CFlock;
+    
     while (true) {
         CFlock.MoveAll();
         CFlock.draw();
         usleep(DELAY);
     }
+    
     return 0;
 }
+
 // -------------------- Bird implementation --------------------
 Bird::Bird() {
     x = std::rand() % WIDTH;
     y = std::rand() % HEIGHT;
 }
+
 void Bird::move_random() {
     int dx = (std::rand() % 3) - 1; // -1, 0, or 1
     int dy = (std::rand() % 3) - 1;
+    
     x = (x + dx + WIDTH) % WIDTH;
     y = (y + dy + HEIGHT) % HEIGHT;
 }
+
 int Bird::get_x()  {
     return x;
 }
+
 int Bird::GetY()  {
     return y;
 }
+
 // -------------------- CFlock implementation --------------------
 void CFlock::MoveAll() {
     for (int i = 0; i < NUM_mBirds; ++i) {
         mBirds[i].move_random();
     }
 }
+
 void CFlock::draw()  {
     // Clear screen
     std::cout << "\033[2J\033[1;1H";
+    
     // Empty screen buffer
     char screen[HEIGHT][WIDTH];
     for (int row = 0; row < HEIGHT; ++row) {
@@ -68,12 +83,14 @@ void CFlock::draw()  {
             screen[row][col] = ' ';
         }
     }
+    
     // Mark bird positions
     for (int i = 0; i < NUM_mBirds; ++i) {
         int x = mBirds[i].get_x();
         int y = mBirds[i].GetY();
         screen[y][x] = '*';
     }
+    
     // Print screen
     for (int row = 0; row < HEIGHT; ++row) {
         for (int col = 0; col < WIDTH; ++col) {
@@ -81,6 +98,7 @@ void CFlock::draw()  {
         }
         std::cout << '\n';
     }
+    
     std::cout << std::flush;
 }
 
