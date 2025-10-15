24eaa4ecc92119ce51b67d2461110830cbc167fb
Donald Dansereau <donald.dansereau@gmail.com>
2025-10-15 12:59:57 +1100

    as coded in-lecture

---
+++ LectureExamples/10B_InheritanceInNodeLive/SensorNodeStartingLive.cpp
@@ -0,0 +1,99 @@
+// A start on the node design for the fire sensor problem
+// See FireSensorProblemStatement.txt
+//
+// In this example we'll use a stand-in CNode that knows how to "publish" by
+// printing to the screen as a stand-in for ROS 2. This allows for lightweight
+// development during the early design stage for this project.
+
+#include "HardwareEmulator.h"
+
+#include <iostream>
+#include <string>
+
+//---Pretend-Node--------------------------------------------------------------
+// A minimalist class to stand in for ROS in small examples
+class CNode
+{
+  public:
+    void Publish( std::string Message );
+};
+
+//-----------------------------------------------------------------------------
+class History
+{
+};
+
+//-----------------------------------------------------------------------------
+class FireDetectorNode: public ISensorCallable, public CNode
+{
+  public:
+  private:
+    History MyHistory;
+};
+
+//-----------------------------------------------------------------------------
+class FireDetectorPN: public FireDetectorNode
+{
+  public:
+    void Callback( int Value )
+    {
+      std::cout << "PN got value " << Value << std::endl;
+      Publish( "PN Received" );
+    }
+};
+
+//-----------------------------------------------------------------------------
+class FireDetectorCam: public FireDetectorNode
+{
+  public:
+    void Callback( int Value )
+    {
+      std::cout << "Cam got value " << Value << std::endl;
+      Publish( "Cam Received" );      
+    }
+};
+
+//-----------------------------------------------------------------------------
+int main()
+{
+  int Choice = 0;
+  std::cout << "What type? [0,1] " << std::endl;
+  std::cin >> Choice;
+  
+  FireDetectorNode* pDetector = NULL;
+  
+  switch( Choice )
+  {
+    case 0:
+      pDetector = new FireDetectorCam;
+    break;
+    case 1:
+      pDetector = new FireDetectorPN;    
+    break;
+    default:
+      std::cout << "oops" << std::endl;
+  }
+
+
+  if( pDetector != NULL )
+  {
+    CHardwareEmulator MyEmulator( *pDetector );
+    // wait for a bit to let some events happen
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+  }  
+
+  if( pDetector != NULL )
+  {
+    delete pDetector;
+  }
+
+
+  return 0;
+}
+
+
+//-----------------------------------------------------------------------------
+void CNode::Publish( std::string Message )
+{
+  std::cout << Message << std::endl;
+}
